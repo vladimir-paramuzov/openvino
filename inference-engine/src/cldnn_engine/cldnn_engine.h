2926b2f29777baf9da8db2bcb61474473d87a46b
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2020-10-23 22:15:50 +0300

    [IE CLDNN] Fixes after rebase

--- inference-engine/src/cldnn_engine/cldnn_engine.h
+++ inference-engine/src/cldnn_engine/cldnn_engine.h
@@ -28,7 +28,7 @@ class clDNNEngine : public InferenceEngine::InferencePluginInternal,
 
     cldnn::device_info GetDeviceInfo(const std::map<std::string, std::string> &config) const;
     InferenceEngine::ICNNNetwork::Ptr CloneAndTransformNetwork(const InferenceEngine::ICNNNetwork& network,
-                                                               CLDNNPlugin::Config config) const;
+                                                               const CLDNNPlugin::Config& config) const;
 public:
     clDNNEngine();
 
