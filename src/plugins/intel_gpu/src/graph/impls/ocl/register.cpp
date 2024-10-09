c437546edde03b6fddc703550b56a2c6bf996f25
Vladimir Paramuzov <vladimir.paramuzov@intel.com>
2024-10-09 15:40:45 +0400

    fixes and dyn rope

--- src/plugins/intel_gpu/src/graph/impls/ocl/register.cpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/register.cpp
@@ -85,7 +85,6 @@ void register_implementations() {
     REGISTER_OCL(unique_count);
     REGISTER_OCL(unique_gather);
     REGISTER_OCL(scaled_dot_product_attention);
-    REGISTER_OCL(rope);
 }
 
 }  // namespace ocl
--- src/plugins/intel_gpu/src/graph/impls/ocl/register.hpp
+++ src/plugins/intel_gpu/src/graph/impls/ocl/register.hpp
@@ -66,7 +66,6 @@
 #include "intel_gpu/primitives/paged_attention.hpp"
 #include "intel_gpu/primitives/kv_cache.hpp"
 #include "intel_gpu/primitives/scaled_dot_product_attention.hpp"
-#include "intel_gpu/primitives/rope.hpp"
 
 namespace cldnn {
 namespace ocl {
@@ -157,7 +156,6 @@ REGISTER_OCL(eye);
 REGISTER_OCL(unique_count);
 REGISTER_OCL(unique_gather);
 REGISTER_OCL(scaled_dot_product_attention);
-REGISTER_OCL(rope);
 
 #undef REGISTER_OCL
 
