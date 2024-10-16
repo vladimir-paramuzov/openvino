323e89c626e7ef54fa9352b15c40c41cc1e41f31
Kim, Eddy <eddy.kim@intel.com>
2024-10-17 04:35:54 +0900

    added a new rt_info scale_factor
    Signed-off-by: Vladimir Paramuzov <vladimir.paramuzov@intel.com>

--- src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
+++ src/plugins/intel_gpu/src/plugin/transformations_pipeline.cpp
@@ -931,8 +931,10 @@ void TransformationsPipeline::apply(std::shared_ptr<ov::Model> func) {
         if (!disable_horizontal_fc_fusion)
             manager.register_pass<ov::pass::ConstantFolding>();
 
-        if (config.get_property(ov::enable_static_scaling))
+        if (config.get_property(ov::enable_static_scaling)) {
+            float scale_factor = func->get_rt_info().count("scale_factor") ? func->get_rt_info<std::float_t>("scale_factor") : 0.f;
             manager.register_pass<ov::pass::StaticScaling>();
+        }
 
         manager.register_pass<ov::pass::ConvertGatherToGatherCompressed>();
         auto pass_config = manager.get_pass_config();
